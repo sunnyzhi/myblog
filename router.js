0df59ddf81ed9972faac6f042dedba252459d8d4
sunzhiqiang <1459079656@qq.com>
2014-09-12 12:37:26 +0800

    Revert "sunny"
    This reverts commit 2d16a95aa7c96ad231506037b46076c84fd152c0.

--- router.js
+++
@@ -1,6 +0,0 @@
-
-/* GET home page. */
-var welcome = require('./controllers/welcome.js');
-module.exports = function(app){
-	app.get('/', welcome.index);
-}
--- settings.js
+++
@@ -1,6 +0,0 @@
-
-module.exports = {
-	cookieSecret: 'blogDB',
-	db:'blogDB',
-	host:'localhost'
-};
